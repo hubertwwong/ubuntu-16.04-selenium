942c5f782625b3b1970a0702d3a337e3ba5ad8d5
Hubert Wong <hubert@sweetyhigh.com>
2017-11-05 23:07:07 -0800

    removed the test app

--- zzz/code/nodejs/selenium_prob/selenium_test_for_nuorder/test/specs/hello.test.js
+++
@@ -1,9 +0,0 @@
-const expect = require('chai').expect
-
-describe("First Spec", function() {
-  it("should navigate to the WebdriverIO homepage", function(){
-    browser.url("http://webdriver.io");
-    var pageUrl = browser.getUrl();
-    expect(pageUrl).to.equal("http://webdriver.io/");
-  });
-});
